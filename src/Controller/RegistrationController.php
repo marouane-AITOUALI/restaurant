86f01f5a3cb7bfc9921f3336684d5e7bca3de345
AITOUALI-Marouane <aitoualimarouane@gmail.com>
2025-01-18 22:31:41 +0100

    add: mail verification
    add: mail verification

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('no-reply@koulMaghreb.com', 'koulMaghreb Mailer'))
+                    ->from(new Address('aitoualimarouane@gmail.com', 'koulMaghreb Mailer'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
