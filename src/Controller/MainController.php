46c926a66f7a9e9ff6541f1713f06170ca20e43e
Nassim Bouzidia <bouzidianassim@gmail.com>
2025-02-06 22:09:03 +0100

    add promotion in hello

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\EventRepository;
 use App\Repository\TableRepository;
+use App\Repository\PromotionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_hello')]
-    public function index(EventRepository $eventRepository, TableRepository $tableRepository): Response
+    public function index(EventRepository $eventRepository, TableRepository $tableRepository, PromotionRepository $promotionRepository): Response
     {
         $events = $eventRepository->findAll();
+        $promotions = $promotionRepository->findAll();
         $tables = $tableRepository->findBy(['est_reserve' => 0]);
         return $this->render('Hello/hello.html.twig', [
             'events' => $events,
             'tables' => $tables,
+            'promotions' => $promotions,
         ]);
     }
-    
+
     #[Route('/A-propos', name: 'a_propos')]
     public function menu(): Response
     {
@@ -32,4 +35,4 @@ class MainController extends AbstractController
     {
         return $this->render('error/banned.html.twig');
     }
-} 
+}
